5b7103d9fae35e3d2ce0d87f5649794920590419
Firdausfatihul <gakoffical@gmail.com>
2020-05-12 19:33:46 +0700

    p

--- app/src/main/java/com/rifqinaufal/myparcel/MainActivity.java
+++ app/src/main/java/com/rifqinaufal/myparcel/MainActivity.java
@@ -2,7 +2,9 @@ package com.rifqinaufal.myparcel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, 1600);
+
+        Thread timerThread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(1600);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
